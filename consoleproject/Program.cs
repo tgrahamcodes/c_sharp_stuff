9c21c52b21bf427cb44551020e490366b90eddb3
Tom Graham <tgraham@wpi.edu>
2022-05-05 15:47:06 -0400

    mehh

---
+++ consoleproject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft;
+
+namespace hello{
+
+	class test{
+		
+		static void Main(string[] args){
+			Console.WriteLine("This is weird");
+			Console.WriteLine(System.PlatformID.MacOSX.ToString());
+			Console.WriteLine(System.PlatformID.Xbox.GetType());
+			if (1 == 1){
+				Console.WriteLine(Microsoft.Win32.RegistryView.Registry64.GetHashCode());
+			}
+		}
+	}
+}
+	
